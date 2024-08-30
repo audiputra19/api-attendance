7cc38c19c0cfe439f726cda4b253c368c9b20c00
audi putra <audiputra19@gmail.com>
2024-08-30 08:06:06 +0700

    edit app.ts

--- src/app.ts
+++ src/app.ts
@@ -28,7 +28,7 @@ app.get('/', (req: Request, res: Response) => {
     res.send("welcome");
 })
 
-const port = process.env.PORT || 3001;
+const port = process.env.PORT
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
 })
