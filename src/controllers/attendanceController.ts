b8ed49165c45d892486ad2ef44d1c6f9e3d9aa98
audi putra <audiputra19@gmail.com>
2024-08-30 08:52:56 +0700

    a# Please enter the commit message for your changes. Lines starting

--- src/controllers/attendanceController.ts
+++ src/controllers/attendanceController.ts
@@ -118,7 +118,7 @@ export const attendanceUser = async (req: Request, res: Response) => {
             const telat = dtAbsen.tlt;
             const telatIst = dtAbsen.tlt_is;
             const alpa = dtAbsen.alpa;
-            console.log(istKeluar);
+            console.log(time);
 
             if(time < istKeluar){
                 message = 'Belum waktunya istirahat';
